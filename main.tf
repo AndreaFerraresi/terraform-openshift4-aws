c8379b910fdfc3e7f26d35604b18891f48cbdc67
cizquierdo <carlos.izquierdo@servexternos.gruposantander.com>
2020-08-24 16:40:54 +0200

    CHG: added simple instance test

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+provider "aws" {
+  version = "~> 2.70.0"
+  region = "eu_west-1"
+}
+
 variable "name_length" {
   description = "The number of words"
   default     = 3
@@ -17,4 +22,30 @@ resource "random_pet" "pet_name" {
 
 output "pet_name" {
   value = random_pet.pet_name.id
+}
+
+# Extract the AMI id
+data "aws_ami" "ami_id" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "description"
+    values = ["Amazon Linux AMI*"]
+  }
+}
+
+# Deploy a simple instance
+resource "aws_instance" "simple_instance" {
+  ami           = data.aws_ami.ami_id.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "basic-test-TFE"
+  }
 }

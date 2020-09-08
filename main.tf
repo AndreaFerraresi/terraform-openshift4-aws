daab801970200f5444ba99f7dca075fb7396736b
FERRARESI <x859988@santanderglobaltech.com>
2020-09-08 11:41:15 +0200

    IBM repo clone

--- main.tf
+++ main.tf
@@ -1,102 +1,136 @@
-# variable "name_length" {
-#   description = "The number of words"
-#   default     = 3
-# }
-
-# variable "keeper_value" {
-#   default = "b"
-# }
-
-# resource "random_pet" "pet_name" {
-#   keepers = {
-#     generate = "${var.keeper_value}"
-#   }
-#   length    = var.name_length
-#   separator = "-"
-# }
-
-# output "pet_name" {
-#   value = random_pet.pet_name.id
-# }
-
-#####
-##### Simple AWS integration
+locals {
+  tags = merge(
+    {
+      "kubernetes.io/cluster/${var.cluster_id}" = "owned"
+    },
+    var.aws_extra_tags,
+  )
+}
 
 provider "aws" {
-  version = "~> 2.70.0"
-  region  = var.region
+  region = var.aws_region
+
+  # Validation of AWS Bahrain region was added in AWS TF provider v2.22
+  # so we skip when installing in me-south-1.
+  skip_region_validation = var.aws_region == "me-south-1"
 }
 
-# Deploy a simple instance
-resource "aws_instance" "simple_instance" {
-  ami           = data.aws_ami.ami_id.id
-  instance_type = var.instance_type
-  subnet_id     = var.account_subnet
-  security_groups = [
-    aws_security_group.foo_sg.id
-  ]
+module "iam" {
+  source = "./iam"
 
-  tags = var.tag_list
+  cluster_id = var.cluster_id
+
+  tags = local.tags
 }
 
-# Create a S3 bucket
-resource "aws_s3_bucket" "bucket_test" {
-  #bucket = "ccc-test-bucket"
-  acl    = "private"
+module "installer" {
+  source = "./install"
 
-  tags = var.tag_list
+  ami = aws_ami_copy.main.id
+  dns_public_id = module.dns.public_dns_id
+  infrastructure_id = var.cluster_id
+  clustername = var.clustername
+  domain = var.base_domain
+  aws_region = var.aws_region
+  aws_access_key_id = var.aws_access_key_id
+  aws_secret_access_key = var.aws_secret_access_key
+  vpc_cidr_block = var.machine_cidr
+  master_count = length(var.aws_azs)
+  openshift_pull_secret = var.openshift_pull_secret
+  openshift_installer_url = var.openshift_installer_url
+  aws_worker_root_volume_iops = var.aws_worker_root_volume_iops
+  aws_worker_root_volume_size = var.aws_worker_root_volume_size
+  aws_worker_root_volume_type = var.aws_worker_root_volume_type
+  aws_worker_availability_zones = var.aws_azs
+  aws_worker_instance_type = var.aws_worker_instance_type
+  airgapped = var.airgapped
 }
 
-# module "eac.aws.modules.ec2-sa" {
-#   source = "eac.aws.modules.ec2-sa"
+module "vpc" {
+  source = "./vpc"
 
-#   region               = var.region
-#   vpc_id               = var.account_vpc
-#   ami                  = data.aws_ami.ami_id.id
-#   ec2_os               = var.ec2_os
-#   instance_type        = var.instance_type
-#   iam_instance_profile = var.iam_instance_profile
-#   key_name             = aws_key_pair.ssh_key.id
-#   subnet_id            = var.account_subnet
-#   security_group_ids   = [aws_security_group.foo_sg.id]
-#   root_volume_type     = var.root_volume_type
-#   kms_key_arn          = aws_kms_key.test-kms-key.arn
-#   cost_center          = var.cost_center
-#   entity               = var.entity
-#   app_name             = var.app_name
-#   product              = var.product
-#   cia                  = var.cia
-#   function             = var.function
-#   channel              = var.channel
-#   description          = var.description
-#   geo_region           = var.geo_region
-#   sequence             = var.secuence
-#   environment          = var.environment
-#   tracking_code        = var.tracking_code
-# }
+  cidr_block       = var.machine_cidr
+  cluster_id       = var.cluster_id
+  region           = var.aws_region
+  vpc              = var.aws_vpc
+  public_subnets   = var.aws_public_subnets
+  private_subnets  = var.aws_private_subnets
+  publish_strategy = var.aws_publish_strategy
+  airgapped = var.airgapped
+  availability_zones = var.aws_azs
 
-# module "eac.aws.modules.as3-sa" {
+  tags = local.tags
+}
 
-#   source = "<bucket s3 module source>"
+module "dns" {
+  source = "./route53"
 
-#   cost_center   = var.cost_center
-#   entity        = var.entity
-#   entity_code   = var.entity_code
-#   app_name      = var.app_name
-#   product       = var.product
-#   cia           = var.cia
-#   function      = var.function
-#   channel       = var.channel
-#   description   = var.description
-#   region        = var.region
-#   geo_region    = var.geo_region
-#   secuence      = var.secuence
-#   environment   = var.environment
-#   tracking_code = var.tracking_code
-#   force_destroy = var.force_destroy
-#   project       = var.project
-#   vpc_id        = var.vpc_id
-#   kms_key_arn   = var.aws_kms_key_arn
-#   log_bucket    = var.log_bucket
+  api_external_lb_dns_name = module.vpc.aws_lb_api_external_dns_name
+  api_external_lb_zone_id  = module.vpc.aws_lb_api_external_zone_id
+  api_internal_lb_dns_name = module.vpc.aws_lb_api_internal_dns_name
+  api_internal_lb_zone_id  = module.vpc.aws_lb_api_internal_zone_id
+  base_domain              = var.base_domain
+  cluster_domain           = "${var.clustername}.${var.base_domain}"
+  cluster_id               = var.cluster_id
+  etcd_count               = length(var.aws_azs)
+  etcd_ip_addresses        = flatten(module.masters.ip_addresses)
+  tags                     = local.tags
+  vpc_id                   = module.vpc.vpc_id
+  publish_strategy         = var.aws_publish_strategy
+}
 
-# }
+resource "aws_ami_copy" "main" {
+  name              = "${var.cluster_id}-master"
+  source_ami_id     = var.aws_ami
+  source_ami_region = var.aws_region
+  encrypted         = true
+
+  tags = merge(
+    {
+      "Name"         = "${var.cluster_id}-master"
+      "sourceAMI"    = var.aws_ami
+      "sourceRegion" = var.aws_region
+    },
+    local.tags,
+  )
+}
+
+module "bootstrap" {
+  source = "./bootstrap"
+
+  ami                      = aws_ami_copy.main.id
+  instance_type            = var.aws_bootstrap_instance_type
+  cluster_id               = var.cluster_id
+  ignition                 = module.installer.bootstrap_ign
+  subnet_id                = var.aws_publish_strategy == "External" ? module.vpc.az_to_public_subnet_id[var.aws_azs[0]] : module.vpc.az_to_private_subnet_id[var.aws_azs[0]]
+  target_group_arns        = module.vpc.aws_lb_target_group_arns
+  target_group_arns_length = module.vpc.aws_lb_target_group_arns_length
+  vpc_id                   = module.vpc.vpc_id
+  vpc_cidrs                = module.vpc.vpc_cidrs
+  vpc_security_group_ids   = [module.vpc.master_sg_id]
+  publish_strategy         = var.aws_publish_strategy
+
+  tags = local.tags
+}
+
+module "masters" {
+  source = "./master"
+
+  cluster_id    = var.cluster_id
+  instance_type = var.aws_master_instance_type
+
+  tags = local.tags
+
+  availability_zones       = var.aws_azs
+  az_to_subnet_id          = module.vpc.az_to_private_subnet_id
+  instance_count           = length(var.aws_azs)
+  master_sg_ids            = [module.vpc.master_sg_id]
+  root_volume_iops         = var.aws_master_root_volume_iops
+  root_volume_size         = var.aws_master_root_volume_size
+  root_volume_type         = var.aws_master_root_volume_type
+  target_group_arns        = module.vpc.aws_lb_target_group_arns
+  target_group_arns_length = module.vpc.aws_lb_target_group_arns_length
+  ec2_ami                  = aws_ami_copy.main.id
+  user_data_ign            = module.installer.master_ign
+  publish_strategy         = var.aws_publish_strategy
+}
